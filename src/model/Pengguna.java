5bd000a991a3dcff1e95c29a9a7fede95becc987
Alif Ihsan <49711087+alifihsan7@users.noreply.github.com>
2025-05-22 12:51:21 +0700

    Create Pengguna.java
    Tambah class pengguna

---
+++ src/model/Pengguna.java
@@ -0,0 +1,30 @@
+package src.model;
+
+public class Pengguna {
+    private String alamat;
+    private String no_telp;
+    
+    public void ProfilePengguna(){
+        
+    }
+    
+    public void BeliBuku(){
+        
+    }
+    
+    public void CariBuku(){
+        
+    }
+    
+    public void RiwayatPembelian(){
+        
+    }
+    
+    public void DaftarBukuDimiliki(){
+        
+    }
+    
+    public void ReviewBuku(){
+        
+    }
+}
