fb4972d1e5803012184540914f32b18345c5897e
Yudha Setiawan <yudha.sw2006@gmail.com>
2025-05-24 15:28:12 +0700

    Update AksesWeb.java

--- src/model/AksesWeb.java
+++ src/model/AksesWeb.java
@@ -1,12 +1,26 @@
-package src.model;
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+/**
+ *
+ * @author yudha
+ */
 public class AksesWeb {
-    public String username;
-    public String password;
-
+    private String username;
+    private String password;
+    private String confirmPassword;
+    
     public AksesWeb(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+    
+    public AksesWeb(String username, String password, String confirmPassword) {
+        this.username = username;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
     }
 
     public void setUsername(String username) {
@@ -25,11 +39,61 @@ public class AksesWeb {
         return password;
     }
 
-    public void login() {
-        
+    public String login() {
+        String status = "Login berhasil";
+        try {
+            JDBC db = new JDBC();
+            String cekUsername = db.getData("SELECT username FROM pengguna WHERE username ='" + username + "';", "username");
+            String cekPassword = db.getData("SELECT password FROM pengguna WHERE username ='" + username + "';", "password");
+            
+            //Memeriksa apakah username ada di DB
+            if (cekUsername != null && "admin".equals(cekUsername.trim())){
+                status = "admin";
+            } else if (cekUsername != null){                  
+                status = "Login berhasil";         
+            } else {
+                status = "Username salah";
+            }
+            
+            //Memeriksa apakah password benar
+            if (cekPassword != null && !cekPassword.trim().equals(password.trim())) {
+                status = "Password salah"; 
+            }
+
+            System.out.println("STATUS: " + status +"\n"
+                    + "cekPASSWORD: "+cekPassword +"\n PASSWORD: "+password 
+                    + "\n cekUSERNAME: "+cekUsername +"\n USERNAME: "+username);
+        } catch(Exception e){
+            status = "Terjadi kesalahan sistem";
+        }
+        return status;  
     }
 
-    public void register() {
-        
+    public String register() {
+        String statusR = "Register berhasil";
+        try {
+            JDBC db = new JDBC();
+            String cekUsername = db.getData("SELECT username FROM pengguna WHERE username ='" + username + "';", "username");
+            
+            if (cekUsername == null){ //Username belum terdaftar di DB
+                if (password.equals(confirmPassword)){ //Password dan confirm password sama
+                    db.runQuery("INSERT pengguna(username, password) VALUES ('"+username+"','"+password+"');");
+                    System.out.println("INSERT pengguna(username, password) VALUES ('"+username+"','"+password+"');");
+                    statusR = "Register berhasil";
+                } else { //Password dan confirm password tidak sama
+                    statusR = "password dan konfirmasi password tidak sama";
+                }
+            } else { //Username sudah digunakan
+                statusR = "Username sudah terdaftar";
+            }
+            
+            System.out.println("STATUS: " + statusR +"\n"
+                    +"\n PASSWORD: "+password+ "\n CONFIRM PASSWORD: " + confirmPassword
+                    + "\n cekUSERNAME: "+cekUsername +"\n USERNAME: "+username);
+        } catch (Exception e) {
+            statusR = e.getMessage();
+        }
+        return statusR;
     } 
 }
+
