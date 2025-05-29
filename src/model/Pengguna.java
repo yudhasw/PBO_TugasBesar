fbad1f49c228b57c77096a8f529fe1b1e732d2a0
Alif Ihsan <49711087+alifihsan7@users.noreply.github.com>
2025-05-29 14:00:21 +0700

    Update Pengguna.java

--- src/model/Pengguna.java
+++ src/model/Pengguna.java
@@ -1,11 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package src.model;
 
+/**
+ *
+ * @author GAMING 3
+ */
+
+import java.sql.*;
+
 public class Pengguna {
-    private String alamat;
-    private String no_telp;
-    
-    public void ProfilePengguna(){
-        
+
+    private String username;
+    private String password;
+
+    public Pengguna(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String updateUsername(String newUsername) {
+        String statusR = "Update username berhasil";
+        try {
+            JDBC db = new JDBC();
+            String cekUsername = db.getData("SELECT username FROM pengguna WHERE username ='" + newUsername + "';", "username");
+
+            if (cekUsername == null) {
+                String updateQuery = "UPDATE pengguna SET username = '" + newUsername + "' WHERE username = '" + this.username + "'";
+                db.runQuery(updateQuery);
+
+                statusR = "Username berhasil diperbarui.";
+                this.username = newUsername;
+            } else {
+                statusR = "Username sudah terdaftar.";
+            }
+        } catch (Exception e) {
+            statusR = "Terjadi kesalahan: " + e.getMessage();
+        }
+        return statusR;
+    }
+
+    public String updatePassword(String newPassword) {
+        String statusR = "Update password berhasil";
+        try {
+            if (newPassword != null && !newPassword.isEmpty()) {
+                JDBC db = new JDBC();
+                String updateQuery = "UPDATE pengguna SET password = '" + newPassword + "' WHERE username = '" + this.username + "'";
+                db.runQuery(updateQuery);
+
+                statusR = "Password berhasil diperbarui.";
+                this.password = newPassword;
+            } else {
+                statusR = "Password baru tidak valid.";
+            }
+        } catch (Exception e) {
+            statusR = "Terjadi kesalahan: " + e.getMessage();
+        }
+        return statusR;
     }
     
     public void BeliBuku(){
